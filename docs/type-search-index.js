4ae0a600718486c521c9419709828c0cb562c5f8
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-02-10 17:00:44 +0100

    actualizacion

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.Amestoy.Libreria","l":"Calculadora"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.Amestoy.Libreria.Calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
