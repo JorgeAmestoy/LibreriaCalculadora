b3652f180c256324d3dce17cf276a3f16ba413bb
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-02-10 17:03:19 +0100

    Creo codigo clase Main para llamar

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,25 @@ import com.Amestoy.Libreria.Calculadora;
 public class Main {
     public static void main(String[] args) {
 
+        int device;
+        Float numero1, numero2;
+        Float resultado;
+        device = EntradaSalida.entradaEntero("1 --> Sumar \n2 --> Restar \n3 --> Multiplicar \n4 --> Dividir \n5 --> Salir del programa");
+        numero1 = EntradaSalida.entradaFloat("Escribe primer numero");
+        numero2 = EntradaSalida.entradaFloat("Escribe segundo numero");
+        if (numero1 == null || numero2 == null) {
+            EntradaSalida.salida("Error: Escribe un numero", EntradaSalida.SALIDA_CONSOLA);
+        } else {
+            if (device > 0 && device < 5) {
+                resultado = Calculadora.operacion(device, numero1, numero2);
+                if (Float.isNaN(resultado)) {
+                    EntradaSalida.salida("Error", EntradaSalida.SALIDA_CONSOLA);
+                } else {
+                    EntradaSalida.salida(("Resultado: " + resultado), EntradaSalida.SALIDA_WINDOW);
+                }
+            }
+
+
+        }
     }
 }
